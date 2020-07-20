cd21e02fc0c2535afb5506a4bf6acb9ca25e5289
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-07-20 19:17:38 +0530

    Commented express.js code now

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
-// const renderBanner = require('../src/renderBanner')
-
-// module.exports = (req, res) => {
-//     res.setHeader("Cache-Control", "public, max-age=1800")
-//     res.setHeader("Content-Type", "image/svg+xml")
-//     res.send(renderBanner())
-// }
-
-const express = require('express')
 const renderBanner = require('../src/renderBanner')
-const app = express()
-const port = 5000
 
-app.get('/', (req, res) => res.send(renderBanner()))
+module.exports = (req, res) => {
+    res.setHeader("Cache-Control", "public, max-age=1800")
+    res.setHeader("Content-Type", "image/svg+xml")
+    res.send(renderBanner())
+}
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// const express = require('express')
+// const renderBanner = require('../src/renderBanner')
+// const app = express()
+// const port = 5000
+
+// app.get('/', (req, res) => res.send(renderBanner()))
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
