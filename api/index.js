f12a5fc6e6f9b322b2db7ebdddf30597c34b5a2f
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-12-25 22:11:01 +0530

    Cache, Color, Typing speed

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const renderBanner = require('../src/renderBanner')
 
 module.exports = (req, res) => {
-    res.setHeader("Cache-Control", "public, max-age=1800")
+    res.setHeader("Cache-Control", "no-cache")
     res.setHeader("Content-Type", "image/svg+xml")
     res.send(renderBanner())
 }
--- src/renderBanner.js
+++ src/renderBanner.js
@@ -7,6 +7,7 @@ const strings = [
 
 const BANNER_WIDTH = "100%"
 const BANNER_HEIGHT = "36px"
+const TYPING_SPEED = 100; // Words per minute
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -14,6 +15,7 @@ function getRandomInt(max) {
 
 const renderBanner = () => {
     const text = strings[getRandomInt(strings.length)]
+    const wordCount = text.split(" ").length;
 
     return `
     <svg
@@ -24,17 +26,14 @@ const renderBanner = () => {
     >
         <style>
             .typing {
-                animation: type 3s steps(${text.length});
+                animation: type ${(wordCount*60)/TYPING_SPEED}s steps(${text.length});
                 font: bold 18px 'Courier New', sans-serif;
                 overflow: hidden;
                 white-space: nowrap;
-                border-right: 2px solid black;
+                border-right: 2px solid #000000;
                 display: table-caption;
-                color: #13A10E;
+                color: #3465A4;
                 margin: 0;
-                position: absolute;
-                top: 50%;
-                transform: translateY(-50%);
             }
 
             @keyframes type {
@@ -49,8 +48,8 @@ const renderBanner = () => {
         </style>
 
 
-        <rect x="0" y="0%" width="${BANNER_WIDTH}" height="${BANNER_HEIGHT}" fill="#0C0C0C" rx="0"/>
-        <foreignObject x="10px" y="0" width="${BANNER_WIDTH}" height="${BANNER_HEIGHT}">
+        <rect x="0" y="0%" width="${BANNER_WIDTH}" height="${BANNER_HEIGHT}" fill="#D3D7CF" rx="0"/>
+        <foreignObject x="10px" y="8px" width="${BANNER_WIDTH}" height="${BANNER_HEIGHT}">
             <h1 class="typing" xmlns="http://www.w3.org/1999/xhtml">${text}</h1>
         </foreignObject>
     </svg>
