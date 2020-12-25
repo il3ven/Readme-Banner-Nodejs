d85c9b3bb08fbf55025b7eacbe7071bc20435f2c
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-12-25 22:51:09 +0530

    Update cache-control

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const renderBanner = require('../src/renderBanner')
 
 module.exports = (req, res) => {
-    res.setHeader("Cache-Control", "no-cache")
+    res.setHeader("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
     res.setHeader("Content-Type", "image/svg+xml")
     res.send(renderBanner())
 }
